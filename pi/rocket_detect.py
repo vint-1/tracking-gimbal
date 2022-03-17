18ab253b544712cbb8caf82c4b1a4d67763bcc31
vint <vint@berkeley.edu>
2022-03-17 14:23:34 -0700

    added detection algorithms

--- pi/rocket_detect.py
+++ pi/rocket_detect.py
@@ -11,12 +11,23 @@ from datetime import datetime
 offline_test = True
 debug_timing = False
 
+OPENCV_OBJECT_TRACKERS = {
+    "csrt": cv.TrackerCSRT_create,
+    "kcf": cv.TrackerKCF_create,
+    "mil": cv.TrackerMIL_create,
+    "boosting": cv.legacy.TrackerBoosting_create,
+    "tld": cv.legacy.TrackerTLD_create,
+    "medianflow": cv.legacy.TrackerMedianFlow_create,
+    "mosse": cv.legacy.TrackerMOSSE_create,
+    "goturn": cv.TrackerGOTURN_create
+}
+
 def main(PROCESS_IMG, LIVE_DISPLAY, RECORD_VIDEO, OBJ_COORD):
 
     ref = "lad4"
     mode = "rockets"
     out_dir = "tracking"
-    algo_name = "goturn"
+    algo_name = "csrt+mosse"
     vid_num = 2
 
     if offline_test:
@@ -38,12 +49,14 @@ def main(PROCESS_IMG, LIVE_DISPLAY, RECORD_VIDEO, OBJ_COORD):
     vid_out1 = None
     tEnd = time.time()
 
-    track_x = []
-    track_y = []
-
     lastPrint = time.time()
 
-    tracker = cv.TrackerGOTURN_create()
+    # tracker = OPENCV_OBJECT_TRACKERS[algo_name]()
+    algorithms = algo_name.split("+")
+    trackers = [OPENCV_OBJECT_TRACKERS[algo]() for algo in algorithms]
+
+    track_x = [[] for _ in range(len(trackers))]
+    track_y = [[] for _ in range(len(trackers))]
 
     while vid.isOpened():
 
@@ -64,22 +77,25 @@ def main(PROCESS_IMG, LIVE_DISPLAY, RECORD_VIDEO, OBJ_COORD):
         # detect rocket
         t1 = time.time()
         
-        x,y = 10,10
         if framenum == 1:
             # get first bounding box
             resized_img = cv.resize(img1, None, fx=1/scale_factor, fy=1/scale_factor, interpolation=cv.INTER_AREA)
-            box = cv.selectROI(resized_img, False)
-            box = tuple([round(coord * scale_factor) for coord in box])
-            print(box)
+            box = [(1008, 1336, 108, 452) for _ in trackers]
+            
+            # box = cv.selectROI(resized_img, False)
+            # box = tuple([round(coord * scale_factor) for coord in box])
+            # print(box)
             # cv.destroyAllWindows()
-            is_detect = tracker.init(img1, box)
+            is_detect = [tracker.init(img1, box[i]) for i,tracker in enumerate(trackers)]
         else:
-            is_detect, box = tracker.update(img1)
+            for i, tracker in enumerate(trackers):
+                is_detect[i], box[i] = tracker.update(img1)
 
-        x = box[0] + box[2]//2 if is_detect else -1
-        y = box[1] + box[3]//2 if is_detect else -1
-        track_x.append(x)
-        track_y.append(y)
+        for i in range(len(trackers)):
+            x = box[i][0] + box[i][2]//2 if is_detect[i] else -1
+            y = box[i][1] + box[i][3]//2 if is_detect[i] else -1
+            track_x[i].append(x)
+            track_y[i].append(y)
 
         t2 = time.time()
 
@@ -89,16 +105,19 @@ def main(PROCESS_IMG, LIVE_DISPLAY, RECORD_VIDEO, OBJ_COORD):
             out_img = img1
 
             if (x,y) != (-1,-1):
+                track_colors = [(20,20,255), (255,20,50)]
                 # cv.circle(out_img, tuple((round(x), round(y))) , 0, (0, 255, 0), thickness=-1)
                 # cv.circle(out_img, tuple((round(x), round(y))) , 10, (0, 255, 0), thickness=1)
                 # roi_1, roi_x, roi_y = roi(img1, (round(x),round(y)), 20)
                 # roi_disp = cv.resize(roi_1, None, fx=8, fy=8, interpolation=cv.INTER_AREA)
-                cv.rectangle(out_img,(box[0],box[1]),(box[0]+box[2],box[1]+box[3]),(20,20,255),4)
+                for i in range(len(trackers)):
+                    cv.rectangle(out_img,(int(box[i][0]),int(box[i][1])),(int(box[i][0]+box[i][2]),int(box[i][1]+box[i][3])),track_colors[i%len(track_colors)],6)
+                    cv.putText(out_img,f"{algorithms[i]}",(50,150+50*(i)),cv.FONT_HERSHEY_DUPLEX,2,track_colors[i],2)
             else:
                 roi_disp = None
 
             out_img_disp = cv.resize(out_img, None, fx=1/scale_factor, fy=1/scale_factor, interpolation=cv.INTER_AREA)
-            cv.putText(out_img_disp,"{:.2f} fps".format(1/(t0-tEnd)),(10,25),cv.FONT_HERSHEY_COMPLEX,0.5,(25,255,255),1)
+            cv.putText(out_img_disp,"{:.2f} fps".format(1/(t0+1e-6-tEnd)),(10,25),cv.FONT_HERSHEY_COMPLEX,0.5,(25,255,255),1)
 
         if live_display_flag:
             cv.imshow("tracking", out_img_disp)
@@ -117,10 +136,10 @@ def main(PROCESS_IMG, LIVE_DISPLAY, RECORD_VIDEO, OBJ_COORD):
             if vid_out1 is None:
                 w, h = out_img.shape[:2]
                 if offline_test:
-                    vid_out1 = cv.VideoWriter(os.path.join(pathutils.media_path, out_dir, f"{ref}-track-{algo_name}-{vid_num}.avi"), fourcc, 20.0, (h, w))
+                    vid_out1 = cv.VideoWriter(os.path.join(pathutils.media_path, f"{mode}", out_dir, f"{ref}-track-{algo_name}-{vid_num}.avi"), fourcc, 30.0, (h, w))
                 else:
                     timestamp = datetime.now().strftime("%y-%m-%d")
-                    vid_out1 = cv.VideoWriter(os.path.join(pathutils.media_path, out_dir, f"{timestamp}-{ref}-track-{algo_name}.avi"), fourcc, 20.0, (h, w))
+                    vid_out1 = cv.VideoWriter(os.path.join(pathutils.media_path, f"{mode}", out_dir, f"{timestamp}-{ref}-track-{algo_name}.avi"), fourcc, 20.0, (h, w))
                 print(vid_out1.isOpened(), out_img.shape[:2])
 
             # if vid_out2 is None:
@@ -160,10 +179,14 @@ def main(PROCESS_IMG, LIVE_DISPLAY, RECORD_VIDEO, OBJ_COORD):
     plt.subplots_adjust(0.05,0.1,0.95,0.9)
     axes[0].set_title(f"x position - {algo_name}")
     axes[0].set_xlabel("frame number")
-    axes[0].plot(track_x)
+    for i, track in enumerate(track_x):
+        axes[0].plot(track, label = f"{algorithms[i]}")
+    axes[0].legend()
     axes[1].set_title(f"y position - {algo_name}")
     axes[1].set_xlabel("frame number")
-    axes[1].plot(track_y)
+    for i, track in enumerate(track_y):
+        axes[1].plot(track, label = f"{algorithms[i]}")
+    axes[1].legend()
 
     plt.show()
 
@@ -195,4 +218,4 @@ def plot_img(img, ax = None, is_show = False, title = "", **kwargs):
 
 
 if __name__ == "__main__":
-    main(True, True, False, False)
+    main(PROCESS_IMG = True, LIVE_DISPLAY = True, RECORD_VIDEO = True, OBJ_COORD = False)
